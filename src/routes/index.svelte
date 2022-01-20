13bd190bf2fb7779d0fa7581510cb52a5c5bd5e7
Massimo Meert <massimomeert@protonmail.com>
2022-01-20 21:28:46 +0100

    changed index text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Guessera</h1>
+<p>A geography quizz made for the <a href="https://www.hack4pan.com/">hack4pan</a> hackaton!</p>
