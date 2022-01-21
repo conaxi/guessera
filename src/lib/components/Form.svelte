071588c55c5ffe2b16036075f92f406ca5f99670
Cyrus <sapweb@protonmail.com>
2022-01-21 20:26:50 +0100

    #3 - Added styles, fixed globe and overall improvement

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,26 @@
+<script>
+	let selected = '20';
+
+	function onChange(event) {
+		selected = event.currentTarget.value;
+	}
+</script>
+
+<div class="container">
+	<label>
+		<input checked={selected === 10} on:change={onChange} type="radio" name="amount" value="10" /> 10
+	</label>
+	<label>
+		<input checked={selected === 20} on:change={onChange} type="radio" name="amount" value="20" /> 20
+	</label>
+	<label>
+		<input checked={selected === 30} on:change={onChange} type="radio" name="amount" value="30" /> 30
+	</label>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
